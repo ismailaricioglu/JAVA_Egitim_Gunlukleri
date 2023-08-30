d828dba037d83a196b61aa0119ad24d4bf50f1a1
ismail arıcıoğlu <93845776+ismailaricioglu@users.noreply.github.com>
2023-08-30 17:46:12 +0300

    Add files via upload

---
+++ example/Classes/ExamplesOfInheritanceByPolymorphismOfBase.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ExamplesOfInheritanceByPolymorphismOfBase {
+    public void MethodOfBase() {
+        System.out.println("This class is Base class");
+    }
+}
---
+++ example/Classes/ExamplesOfInheritanceByPolymorphismOfSubFirstClass.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ExamplesOfInheritanceByPolymorphismOfSubFirstClass extends ExamplesOfInheritanceByPolymorphismOfBase {
+    public void MethodOfBase() {
+        System.out.println("This class is Sub Frist class");
+    }
+}
